7315c1c0773709b01ee6402b2de11dc6824db332
Ayukey <yukeyjc@gmail.com>
2018-11-18 14:28:30 +0800

    no message

---
+++ utils/httpclient.js
@@ -0,0 +1,41 @@
+const regeneratorRuntime = require('regenerator-runtime')
+
+const isDev = true
+
+const Host = isDev ? "http://localhost:8080/" : "https://scapi.sh2j.com/"
+
+const Api = {
+  UserLogin: `${Host}UserInfo/UserLogin`,
+  GetCurrentQuarter: `${Host}AppCommon/GetCurrentQuarter`,
+};
+
+// 微信请求
+const wxRequest = async(url, method = "GET", data = {}) => {
+  return await new Promise(function(resolve, reject) {
+    wx.request({
+      url: url,
+      data: data,
+      method: method,
+      success: function(res) {
+        console.log(res)
+        if (res.statusCode == 200) {
+          if (res.data.Success) {
+            resolve(res.data.Data)
+          } else {
+            reject(res.data.Msg)
+          }
+        } else {
+          reject(res.errMsg)
+        }
+      },
+      fail: function(err) {
+        reject(err)
+      }
+    })
+  })
+}
+
+module.exports = {
+  Api,
+  wxRequest,
+}
